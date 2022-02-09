141d2eb22eda16f81b64037b5217c539147f03cd
MaryamGaffar <maayam.2019111@iit.ac.lk>
2022-02-09 15:48:08 +0530

    Added URLS

--- DSGP6/PlanetHunters_Code/planethunters/api/urls.py
+++ DSGP6/PlanetHunters_Code/planethunters/api/urls.py
@@ -7,4 +7,26 @@ urlpatterns = [
     path('stars/<int:id>/', StarDetails.as_view()),
     path('users/', UserList.as_view()),
     path('users/<str:id>/', UserDetails.as_view()),
+    ##path('home/',home.html) 
 ]
+For Website
+
+https://www.srilankanplanethunters.com/home
+https://www.srilankanplanethunters.com/about
+https://www.srilankanplanethunters.com/features
+https://www.srilankanplanethunters.com/blog
+
+For dashboard
+
+dashboard/home
+dashboard/search
+dashboard/filter
+dashboard/analyze
+dashboard/machinelearning
+dashboard/habitability
+dashboard/profile
+dashboard/signin
+dashboard/register
+dashboard/forgetpwd
+
+
