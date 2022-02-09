eb8261240b7c253b9102bf1d6ae7f398a0cc9bb8
MaryamGaffar <maayam.2019111@iit.ac.lk>
2022-02-09 20:37:16 +0530

    added urls

--- DSGP6/PlanetHunters_Code/planethunters/api/urls.py
+++ DSGP6/PlanetHunters_Code/planethunters/api/urls.py
@@ -8,25 +8,21 @@ urlpatterns = [
     path('users/', UserList.as_view()),
     path('users/<str:id>/', UserDetails.as_view()),
     ##path('home/',home.html) 
+    ##path('about/', about.html)
+    ##path('features/',features.html)
+    ##path('blog/',blog.html)
+    
+    ##path('dashboard/home/',home.html)
+    ##path('dashboard/search/',search.html)
+    ##path('dashboard/filter/',filter.html)
+    ##path('dashboard/analyze/',analyze.html)
+    ##path('dashboard/machinelearning/',machinelearning.html)
+    ##path('dashboard/habitability/',habitability.html)
+    ##path('dashboard/profile/',profile.html)
+    ##path('dashboard/signin/',signin.html)
+    ##path('dashboard/register/',register.html)
+    ##path('dashboard/forgetpwd/',forgetpwd.html)
+
 ]
-For Website
-
-https://www.srilankanplanethunters.com/home
-https://www.srilankanplanethunters.com/about
-https://www.srilankanplanethunters.com/features
-https://www.srilankanplanethunters.com/blog
-
-For dashboard
-
-dashboard/home
-dashboard/search
-dashboard/filter
-dashboard/analyze
-dashboard/machinelearning
-dashboard/habitability
-dashboard/profile
-dashboard/signin
-dashboard/register
-dashboard/forgetpwd
 
 
