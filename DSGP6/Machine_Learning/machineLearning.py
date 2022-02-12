e9a0c4e5acf8e9d80f77a016a4ab3d327dfffed5
MaryamGaffar <maayam.2019111@iit.ac.lk>
2022-02-12 09:37:43 +0530

    added some codes

--- DSGP6/Machine_Learning/machineLearning.py
+++ DSGP6/Machine_Learning/machineLearning.py
@@ -6,3 +6,6 @@ data = pd.read_csv(r'/Users/thayalanpraveen/Documents/GitHub/PlanetHunters/DSGP6
 ##data = pd.read_csv(r'/Users/thayalanpraveen/Documents/GitHub/PlanetHunters/DSGP6/Machine_Learning/Dataset.csv') Maryam dataset location
 print(data)
 
+## statistics of dataset columns
+data_stats = data.describe()
+print(data_stats) 
