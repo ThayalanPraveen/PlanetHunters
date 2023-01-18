27596b65ec074b2842f66b68548f21df52e6ed45
Hamdaan Mohideen <86551629+HamdaanMohideen@users.noreply.github.com>
2023-01-19 02:39:31 +0530

    fixed the habitability window font sizes

--- DSGP6/Application/Product_GUI/PlanetHuntersGUI_2.0.py
+++ DSGP6/Application/Product_GUI/PlanetHuntersGUI_2.0.py
@@ -3293,7 +3293,7 @@ class Habitability(QWidget):
         # Period label in the Exo-Planet Detection screen
         # --------------------------------------------------------------------------
         self.period_label = QLabel("Enter Period :", self)
-        self.period_label.setFont(QFont(app_font,12))
+        # self.period_label.setFont(QFont(app_font,12))
         self.period_label.setGeometry(460,30,100,30)
         # --------------------------------------------------------------------------
         # period input textbox in the Exo-Planet Detection screen
@@ -3314,7 +3314,7 @@ class Habitability(QWidget):
         # Period label in the Exo-Planet Detection screen
         # --------------------------------------------------------------------------
         self.epoch_label = QLabel("Enter Transit Epoch (t0) :", self)
-        self.epoch_label.setFont(QFont(app_font,12))
+        # self.epoch_label.setFont(QFont(app_font,12))
         self.epoch_label.setGeometry(460,100,150,30)
         # --------------------------------------------------------------------------
         # period input textbox in the Exo-Planet Detection screen
@@ -3352,12 +3352,11 @@ class Habitability(QWidget):
         # ------------------------------------------------------------------------
 
         self.slider_label = QLabel("Adjust axhline :", self)
-        self.slider_label.setFont(QFont(app_font,12))
+        # self.slider_label.setFont(QFont(app_font,12))
         self.slider_label.setStyleSheet("color: #"+button_hover_hex)
         self.slider_label.setGeometry(460,210,150,30)
 
         self.slider = QSlider(Qt.Horizontal,self)
-        self.slider.setMinimum(ylim_min*1000000)
         self.slider.setMaximum(ylim_max*1000000)
         self.slider.setSingleStep(1)
         self.slider.setGeometry(460,230,150,30)
@@ -3365,16 +3364,16 @@ class Habitability(QWidget):
         self.slider.sliderMoved.connect(self.slider_position)
 
         self.y_label = QLabel("Adjust Y limits :", self)
-        self.y_label.setFont(QFont(app_font,12))
+        # self.y_label.setFont(QFont(app_font,12))
         self.y_label.setStyleSheet("color: #"+button_hover_hex)
         self.y_label.setGeometry(460,260,150,20)
 
         self.y_min = QLabel("Y min :", self)
-        self.y_min.setFont(QFont(app_font,12))
+        # self.y_min.setFont(QFont(app_font,12))
         self.y_min.setGeometry(460,290,150,20)
 
         self.y_min_input = QLineEdit(self)
-        self.y_min_input.setFont(QFont(app_font,13))
+        # self.y_min_input.setFont(QFont(app_font,13))
         self.y_min_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3386,11 +3385,11 @@ class Habitability(QWidget):
         self.y_min_input.setAlignment(Qt.AlignCenter)
 
         self.y_max = QLabel("Y max :", self)
-        self.y_max.setFont(QFont(app_font,12))
+        # self.y_max.setFont(QFont(app_font,12))
         self.y_max.setGeometry(460,320,150,30)
 
         self.y_max_input = QLineEdit(self)
-        self.y_max_input.setFont(QFont(app_font,13))
+        # self.y_max_input.setFont(QFont(app_font,13))
         self.y_max_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3403,7 +3402,7 @@ class Habitability(QWidget):
 
         self.y_btn = QPushButton(self)
         self.y_btn.setIcon(PySide6.QtGui.QIcon(os.path.join(sys.path[0],'Images/refresh.png')))
-        self.y_btn.setFont(QFont(app_font,15))
+        # self.y_btn.setFont(QFont(app_font,15))
         self.y_btn.setStyleSheet("""
                                 QPushButton {
                                     border-radius:10px;
@@ -3414,21 +3413,21 @@ class Habitability(QWidget):
                                     color: #000000
                                     }
                                 """)
-        self.y_btn.setGeometry(583,297,40,40)
+        self.y_btn.setGeometry(583,297,30,30)
         self.y_btn.clicked.connect(self.y_axis_change)
 
 
         self.x_label = QLabel("Adjust X limits :", self)
-        self.x_label.setFont(QFont(app_font,12))
+        # self.x_label.setFont(QFont(app_font,12))
         self.x_label.setStyleSheet("color: #"+button_hover_hex)
         self.x_label.setGeometry(460,360,150,20)
 
         self.x_min = QLabel("X min :", self)
-        self.x_min.setFont(QFont(app_font,12))
+        # self.x_min.setFont(QFont(app_font,12))
         self.x_min.setGeometry(460,390,150,20)
 
         self.x_min_input = QLineEdit(self)
-        self.x_min_input.setFont(QFont(app_font,13))
+        # self.x_min_input.setFont(QFont(app_font,13))
         self.x_min_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3440,11 +3439,11 @@ class Habitability(QWidget):
         self.x_min_input.setAlignment(Qt.AlignCenter)
 
         self.x_max = QLabel("X max :", self)
-        self.x_max.setFont(QFont(app_font,12))
+        # self.x_max.setFont(QFont(app_font,12))
         self.x_max.setGeometry(460,420,150,30)
 
         self.x_max_input = QLineEdit(self)
-        self.x_max_input.setFont(QFont(app_font,13))
+        # self.x_max_input.setFont(QFont(app_font,13))
         self.x_max_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3457,7 +3456,7 @@ class Habitability(QWidget):
 
         self.x_btn = QPushButton(self)
         self.x_btn.setIcon(PySide6.QtGui.QIcon(os.path.join(sys.path[0],'Images/refresh.png')))
-        self.x_btn.setFont(QFont(app_font,15))
+        # self.x_btn.setFont(QFont(app_font,15))
         self.x_btn.setStyleSheet("""
                                 QPushButton {
                                     border-radius:10px;
@@ -3468,16 +3467,16 @@ class Habitability(QWidget):
                                     color: #000000
                                     }
                                 """)
-        self.x_btn.setGeometry(583,397,40,40)
+        self.x_btn.setGeometry(583,397,30,30)
         self.x_btn.clicked.connect(self.x_axis_change)
 
         self.star_radius_label = QLabel("Input Star Radius :", self)
-        self.star_radius_label.setFont(QFont(app_font,12))
+        # self.star_radius_label.setFont(QFont(app_font,12))
         self.star_radius_label.setStyleSheet("color: #"+button_hover_hex)
         self.star_radius_label.setGeometry(460,460,150,20)
 
         self.star_radius_input = QLineEdit(self)
-        self.star_radius_input.setFont(QFont(app_font,13))
+        # self.star_radius_input.setFont(QFont(app_font,13))
         self.star_radius_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3489,7 +3488,7 @@ class Habitability(QWidget):
         self.star_radius_input.setAlignment(Qt.AlignCenter)
 
         self.albedo_label = QLabel("Planet Albedo : 0", self)
-        self.albedo_label.setFont(QFont(app_font,12))
+        # self.albedo_label.setFont(QFont(app_font,12))
         self.albedo_label.setStyleSheet("color: #"+button_hover_hex)
         self.albedo_label.setGeometry(630,520,200,20)
 
@@ -3501,12 +3500,12 @@ class Habitability(QWidget):
         self.albedo_slider.sliderMoved.connect(self.albedo_position)
 
         self.star_eff_label = QLabel("Input Star Effective Temp", self)
-        self.star_eff_label.setFont(QFont(app_font,12))
+        # self.star_eff_label.setFont(QFont(app_font,12))
         self.star_eff_label.setStyleSheet("color: #"+button_hover_hex)
         self.star_eff_label.setGeometry(460,520,150,20)
 
         self.star_eff_input = QLineEdit(self)
-        self.star_eff_input.setFont(QFont(app_font,13))
+        # self.star_eff_input.setFont(QFont(app_font,13))
         self.star_eff_input.setStyleSheet("""
                                 QLineEdit {
                                     border-radius:10px;
@@ -3533,57 +3532,57 @@ class Habitability(QWidget):
         self.calc_radius_btn.clicked.connect(self.calc_radius)
 
         self.summary_label = QLabel("-- Summary --", self)
-        self.summary_label.setFont(QFont(app_font,13))
+        # self.summary_label.setFont(QFont(app_font,13))
         self.summary_label.setStyleSheet("color: #"+button_hover_hex)
         self.summary_label.setGeometry(810,460,150,20)
 
         self.summary_period_label = QLabel("Period : Not Set", self)
-        self.summary_period_label.setFont(QFont(app_font,12))
+        # self.summary_period_label.setFont(QFont(app_font,12))
         self.summary_period_label.setGeometry(810,525-40,150,20)
 
         self.summary_epoch_label = QLabel("Transit Epoch (t0) : Not Set", self)
-        self.summary_epoch_label.setFont(QFont(app_font,12))
+        # self.summary_epoch_label.setFont(QFont(app_font,12))
         self.summary_epoch_label.setGeometry(810,545-40,150,20)
 
         self.summary_axhline_label = QLabel("Transit Depth (1 - Axhline) : 1", self)
-        self.summary_axhline_label.setFont(QFont(app_font,12))
+        # self.summary_axhline_label.setFont(QFont(app_font,12))
         self.summary_axhline_label.setGeometry(810,565-40,240,20)
 
         self.summary_star_radius_label = QLabel("Star Radius : Not Set", self)
-        self.summary_star_radius_label.setFont(QFont(app_font,12))
+        # self.summary_star_radius_label.setFont(QFont(app_font,12))
         self.summary_star_radius_label.setGeometry(810,585-40,150,20)
 
         self.summary_star_temp_label = QLabel("Star Eff Temp : Not Set", self)
-        self.summary_star_temp_label.setFont(QFont(app_font,12))
+        # self.summary_star_temp_label.setFont(QFont(app_font,12))
         self.summary_star_temp_label.setGeometry(810,605-40,150,20)
 
         self.summary_planet_albedo_label = QLabel("Planet Albedo : Not Set", self)
-        self.summary_planet_albedo_label.setFont(QFont(app_font,12))
+        # self.summary_planet_albedo_label.setFont(QFont(app_font,12))
         self.summary_planet_albedo_label.setGeometry(810,625-40,150,20)
 
         self.results_label = QLabel("-- Results --", self)
-        self.results_label.setFont(QFont(app_font,13))
+        # self.results_label.setFont(QFont(app_font,13))
         self.results_label.setStyleSheet("color: #"+button_hover_hex)
         self.results_label.setGeometry(1020,520-60,150,20)
 
         self.results_solar_label = QLabel("Radius as Solar Radius : Not Calculated ", self)
-        self.results_solar_label.setFont(QFont(app_font,12))
+        # self.results_solar_label.setFont(QFont(app_font,12))
         self.results_solar_label.setGeometry(1020,545-60,350,20)
 
         self.results_earth_label = QLabel("Radius as Earth Radius : Not Calculated", self)
-        self.results_earth_label.setFont(QFont(app_font,12))
+        # self.results_earth_label.setFont(QFont(app_font,12))
         self.results_earth_label.setGeometry(1020,565-60,350,20)
 
         self.results_axis_label = QLabel("Orbit Semi Major Axis as Solar Radius: Not Calculated", self)
-        self.results_axis_label.setFont(QFont(app_font,12))
+        # self.results_axis_label.setFont(QFont(app_font,12))
         self.results_axis_label.setGeometry(1020,585-60,350,20)
 
         self.results_axis_earth_label = QLabel("Orbit Semi Major Axis as Earth Radius: Not Calculated", self)
-        self.results_axis_earth_label.setFont(QFont(app_font,12))
+        # self.results_axis_earth_label.setFont(QFont(app_font,12))
         self.results_axis_earth_label.setGeometry(1020,605-60,350,20)
 
         self.results_temp_label = QLabel("Equilibrium Temperature : Not Calculated", self)
-        self.results_temp_label.setFont(QFont(app_font,12))
+        # self.results_temp_label.setFont(QFont(app_font,12))
         self.results_temp_label.setGeometry(1020,625-60,350,20)
 
     # Display search results after search complete in Habitability screen
@@ -4068,7 +4067,7 @@ myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 app.setWindowIcon(QIcon(r'Application\Product_GUI\Images\icon.png'))
-app.setStyleSheet("QLabel {color: white;} QLineEdit {color: white;} QRadioButton {color: white;} QCheckBox {color: white} QPushButton {color: white;} QToolButton {color: white;}") # Setting all labels in the app to white color
+app.setStyleSheet("QLabel {color: white;} QLineEdit {color: white;} QRadioButton {color: white;} QCheckBox {color: white} QPushButton {color: white;} QToolButton {color: white;} ") # Setting all labels in the app to white color
 pixmap = QPixmap(os.path.join(sys.path[0],"Images/logo.png"))
 splash = QSplashScreen(pixmap)
 splash.show()
